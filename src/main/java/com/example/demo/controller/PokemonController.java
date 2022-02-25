3c0878b8873df3e0ae55d9351a192a88e5de881d
SeungHa Jeon <seuha516@naver.com>
2022-02-25 14:25:55 +0900

    .

--- src/main/java/com/example/demo/controller/PokemonController.java
+++ src/main/java/com/example/demo/controller/PokemonController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000, https://pokedex-seuha516.netlify.app/")
+@CrossOrigin(origins = "https://pokedex-seuha516.netlify.app/")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/")
